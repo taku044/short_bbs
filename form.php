06f7b1520b636fa305c901d44dd12c615ad8b385
taku044 <takuitiani0416606@gmail.com>
2025-06-06 10:40:56 +0900

    Update form.php

--- form.php
+++ form.php
@@ -1,34 +1,4 @@
-<?php 
-$db = [
-    'host' => "mysql309.phy.lolipop.lan",
-    'user' => "LAA1554882",
-    'pass' => "Pass0503",
-    'dbname' => "LAA1554882-pagepalette"
-];
 
-try {
-    $pdo = new PDO(
-        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'] . ';charset=utf8',
-        $db['user'],
-        $db['pass']
-    );
-} catch (PDOException $e) {
-    die('データベース接続に失敗しました: ' . htmlspecialchars($e->getMessage()));
-}
-if (isset($_POST['username']) && !$_POST['username'] == "") {
-    $user=$_POST['username'];
-    $password=$_POST['password'];
-    $stmt = $pdo->prepare('SELECT password FROM users WHERE username = ?');
-    $stmt->execute([$username]);
-    $user = $stmt->fetch();
-
-    if ($user && password_verify($password, $user['password'])) {
-    } else {
-       header("Location: login.php");
-   }
-}
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -41,7 +11,7 @@ if (isset($_POST['username']) && !$_POST['username'] == "") {
     <form action="post.php" method="post">
         <p>ようこそ、    
         <?php 
-        echo $_POST['username'];
+        echo $_SESSION['username'];
         ?>
         さん</p>
         <p>コメント：<br>
