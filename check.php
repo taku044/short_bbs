9c5c6b81d437b79a4665282cf1d55f28ae1665a4
taku044 <takuitiani0416606@gmail.com>
2025-06-06 10:40:48 +0900

    ログイン機能を移動させた

---
+++ check.php
@@ -0,0 +1,34 @@
+<?php 
+session_start();
+$db = [
+    'host' => "mysql309.phy.lolipop.lan",
+    'user' => "LAA1554882",
+    'pass' => "Pass0503",
+    'dbname' => "LAA1554882-pagepalette"
+];
+
+try {
+    $pdo = new PDO(
+        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'] . ';charset=utf8',
+        $db['user'],
+        $db['pass']
+    );
+} catch (PDOException $e) {
+    die('データベース接続に失敗しました: ' . htmlspecialchars($e->getMessage()));
+}
+if (isset($_POST['username']) && !$_POST['username'] == "") {
+    $user=$_POST['username'];
+    $password=$_POST['password'];
+    $stmt = $pdo->prepare('SELECT password FROM user WHERE username = ?');
+    $stmt->execute([$username]);
+    $userin = $stmt->fetch();
+
+    if ($user && password_verify($password, $userin['password'])) {
+        $_SESSION['username'] = $user;
+        header("Location: form.php");
+    } else {
+       header("Location: login.php");
+   }
+}
+
+?>
