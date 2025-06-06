285137b996c666b4d0dead27ef3726b2bb7fd148
taku044 <takuitiani0416606@gmail.com>
2025-06-06 10:32:05 +0900

    ログイン画面作成

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="form.php"method="post">
+         ユーザー名：
+         <input type="text" name=username ><br>
+         パスワード：
+         <input type="password" name=password ><br>
+         <button type="submit" name="login">ログイン</button>
+    </form>
+   
+    
+</body>
+</html>
